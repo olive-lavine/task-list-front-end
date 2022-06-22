1ddf0892ace770441e9c7f44cdb954d392af11d7
Shari Fedak <90287794+sharifedak12@users.noreply.github.com>
2022-06-22 17:21:15 -0400

    Working on Wave 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-import tasksJson from './data/tasks.json';
+// import tasksJson from './data/tasks.json';
+import axios from 'axios';
+
+const kBaseUrl = 'http://localhost:5000';
+
+const taskApiToJson = (cat) => {
+  const { id, title, is_complete: isComplete } = cat;
+  return { id, title, isComplete };
+};
+
+const getTasks = () => {
+  return axios
+    .get(`${kBaseUrl}/tasks`)
+    .then((response) => {
+      return response.data.map(taskApiToJson);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const markTaskComplete = (id) => {
+  return (
+    axios
+      .patch(`${kBaseUrl}/tasks/${id}/mark_complete`)
+      // .then((response) => {
+      //   return taskApiToJson(response.data);
+      // })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+};
 
 const App = () => {
-  const [tasks, setTasks] = useState(tasksJson);
+  const [tasks, setTasks] = useState([]);
+  const updateTasks = () => {
+    getTasks().then((tasks) => {
+      setTasks(tasks);
+    });
+  };
+
+  useEffect(() => {
+    updateTasks();
+  }, []);
 
   const updateTask = (id) => {
-    const newTasks = tasks.map((task) => {
-      if (task.id === id) {
-        return { ...task, isComplete: !task.isComplete };
-      } else {
-        return task;
-      }
+    markTaskComplete(id).then((updatedTask) => {
+      setTasks((oldData) => {
+        return oldData.map((task) => {
+          if (task.id === id) {
+            return updatedTask;
+          } else {
+            return task;
+          }
+        });
+      });
     });
-
-    setTasks(newTasks);
   };
 
   const deleteTask = (id) => {
